6992bb0ce718db5b64a79faa431a8ea9059434e9
Andreas Fitzek <andreas.fitzek@n8n.io>
2026-04-30 14:30:22 +0200

    Use in debug information and remove UI page

--- packages/frontend/editor-ui/src/app/constants/navigation.ts
+++ packages/frontend/editor-ui/src/app/constants/navigation.ts
@@ -70,7 +70,6 @@ export const enum VIEWS {
 	MIGRATION_REPORT = 'MigrationReport',
 	MIGRATION_RULE_REPORT = 'MigrationRuleReport',
 	RESOLVERS = 'Resolvers',
-	INSTANCE_REGISTRY = 'InstanceRegistryView',
 	RESOURCE_CENTER = 'ResourceCenter',
 }
 
